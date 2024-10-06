cf1fe87ff4c18741a8e2a023484da3314447e871
moataz kamal <moatazkmal@gmail.com>
2024-10-06 14:59:45 +0300

    task 0

--- quiz.js
+++ quiz.js
@@ -1,13 +1,10 @@
 function checkAnswer() {
-  // Correct answer for the quiz
   const correctAnswer = "4";
 
-  // Retrieve the user's selected answer value directly
   const userAnswer = document.querySelector(
     'input[name="quiz"]:checked'
   )?.value;
 
-  // Compare the user's answer with the correct answer
   if (userAnswer === correctAnswer) {
     document.getElementById("feedback").textContent = "Correct! Well done.";
   } else {
@@ -15,7 +12,6 @@ function checkAnswer() {
       "That's incorrect. Try again!";
   }
 
-  // If no option is selected, prompt the user to choose an answer
   if (!userAnswer) {
     document.getElementById("feedback").textContent =
       "Please select an answer.";
